d921dbb20ae9e096b06a23c7d6163b04c0881b38
David Strawn <isomarcte@gmail.com>
2020-02-10 17:21:19 -0700

    squash!

--- src/Vault/Data/Internal/Text.hs
+++ src/Vault/Data/Internal/Text.hs
@@ -1,5 +1,6 @@
 module Vault.Data.Internal.Text
   ( AText(..)
+  , textGen
   ) where
 
 import Control.Arrow ((>>^), (^>>))
@@ -9,7 +10,7 @@ import Data.Semigroup (Semigroup)
 import Data.String (IsString(fromString), String)
 import Data.Text (Text, unpack)
 import Prelude (Eq, Ord, Read, Show)
-import Test.QuickCheck (Arbitrary(arbitrary, shrink))
+import Test.QuickCheck (Arbitrary(arbitrary, shrink), Gen)
 
 newtype AText =
   AText
@@ -23,3 +24,6 @@ unpackAText (AText t) = unpack t
 instance Arbitrary AText where
   arbitrary = fmap fromString arbitrary
   shrink = unpackAText ^>> shrink >>^ fmap fromString
+
+textGen :: Gen Text
+textGen = fmap aTextValue arbitrary
--- src/Vault/Data/V1/Auth/Github.hs
+++ src/Vault/Data/V1/Auth/Github.hs
@@ -8,17 +8,18 @@ import GHC.Generics (Generic)
 import Generic.Random (genericArbitrarySingle)
 import Prelude (Eq, Ord, Read, Show)
 import Test.QuickCheck.Arbitrary (Arbitrary(arbitrary, shrink), genericShrink)
-import Vault.Data.Internal (textGen)
+import Vault.Data.Internal.Text (aTextValue, textGen)
 
-data GithubAuthRequest =
+data GithubAuthRequest t =
   GithubAuthRequest
-    { organization :: Text
-    , base_url :: Maybe Text
+    { organization :: t
+    , base_url :: Maybe t
     }
   deriving (Eq, Read, Ord, Show, Generic)
 
-instance Arbitrary GithubAuthRequest where
+instance Arbitrary t => Arbitrary (GithubAuthRequest t) where
   arbitrary = do
-    organization <- textGen
-    base_url <- 
+    organization <- arbitrary
+    base_url <- arbitrary
+    return GithubAuthRequest {organization = organization, base_url = base_url}
   shrink = genericShrink
