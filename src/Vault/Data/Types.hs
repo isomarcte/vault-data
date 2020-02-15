e80519a261334881a9ddc6d5448bb55d626e29d4
David Strawn <isomarcte@gmail.com>
2020-02-14 17:01:17 -0700

    squash!

--- src/Vault/Data/Types.hs
+++ src/Vault/Data/Types.hs
@@ -1,12 +1,26 @@
 module Vault.Data.Types
   ( URL(..)
-  , TokenPolicy
-  , TokenBoundCidr
+  , TokenPolicy(..)
+  , TokenBoundCidr(..)
+  , RequestId(..)
+  , LeaseId(..)
+  , LeaseDuration(..)
+  , Renewable(..)
+  , VaultWarning(..)
   ) where
 
 import Data.Aeson (FromJSON, ToJSON)
+import Data.Bool (Bool)
+import Data.Eq (Eq)
+import Data.Monoid (Monoid)
+import Data.Ord (Ord)
+import Data.Semigroup (Semigroup)
 import Data.Text (Text)
-import Prelude (Eq, Monoid, Ord, Read, Semigroup, Show)
+import Data.UUID (UUID)
+import Data.Word (Word)
+import Prelude (Bounded, Enum, Integral, Num, Real)
+import Text.Read (Read)
+import Text.Show (Show)
 
 newtype URL =
   URL Text
@@ -18,4 +32,35 @@ newtype TokenPolicy =
 
 newtype TokenBoundCidr =
   TokenBoundCidr Text
+  deriving (Eq, Monoid, Ord, Semigroup, Show, Read, ToJSON, FromJSON)
+
+newtype RequestId =
+  RequestId UUID
+  deriving (Eq, Ord, Show, Read, ToJSON, FromJSON)
+
+newtype LeaseId =
+  LeaseId Text
+  deriving (Eq, Monoid, Ord, Semigroup, Show, Read, ToJSON, FromJSON)
+
+newtype LeaseDuration =
+  LeaseDuration Word
+  deriving ( Eq
+           , Ord
+           , Show
+           , Read
+           , Enum
+           , Num
+           , Real
+           , Bounded
+           , Integral
+           , ToJSON
+           , FromJSON
+           )
+
+newtype Renewable =
+  Renewable Bool
+  deriving (Eq, Ord, Show, Read, Enum, Bounded, ToJSON, FromJSON)
+
+newtype VaultWarning =
+  VaultWarning Text
   deriving (Eq, Monoid, Ord, Semigroup, Show, Read, ToJSON, FromJSON)
